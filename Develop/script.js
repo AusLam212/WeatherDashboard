2f8f9978aa6619084b90d8bd686ae7d514249867
Austin Lambert <atlambert11@gmail.com>
2020-10-05 20:55:24 -0500

    Added click event to store city entered into local storage. Also displays value of text input into newly formed divs.

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,10 @@
-var addCity = $("#add-city");
-var cityInput = $("#city-input");
-
 var cities = [];
 
-
-
-
 function renderCity() {
+    //Clear value on input text
     $("#cities-added").empty();
 
-
-    for (var i = 0; i < cities.length, i++) {
+    for (var i = 0; i < cities.length; i++) {
         //Div for new cities to be inouted into
         var cityDiv = $("<div>");
         //Adding class to new cityDiv
@@ -20,19 +14,49 @@ function renderCity() {
         //Adding text to new cityDiv
         cityDiv.text(cities[i]);
         //Adding text to html
-
+        $("#cities-added").append(cityDiv);
 
 
     }
 }
 
-$(cityInput).click(function() {
 
-
-
-
+$("#add-city").click(function(event) {
+    //Keep page from refreshing on submit button being clicked
+    event.preventDefault();
+    //Grab text from input text box
+    var inputCity = $("#city-input").val().trim();
+    //Push added cities to #cities-added div
+    cities.push(inputCity);
+    //Push to Local Storage
+    localStorage.setItem("cityListItem", inputCity);
+    //Calling renderCity function to add to list
     renderCity();
-})
+    fiveDayForcast(inputCity);
+});
+
+function onPageLoad() {
+    $("#cities-added").text(localStorage.getItem("cityListItem"));
+}
+
+onPageLoad();
+
+
+function fiveDayForcast(city) {
+    var APIkey = "fe80024a92d6b1c9f72576aaefc212e3";
+    var queryURL5Day = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5&appid=" + APIkey;
+    // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
+    // https://api.openweathermap.org/data/2.5/forecast/daily?q=Austin&cnt=5&appid=fe80024a92d6b1c9f72576aaefc212e3
+    $.ajax({
+        url: queryURL5Day,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+
+}
+
+
 
 
 
